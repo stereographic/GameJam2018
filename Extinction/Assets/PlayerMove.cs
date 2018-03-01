030833622772435d103dc4e61db9321e36f51185
Dave-weatherby <dave-weatherby@hotmail.com>
2018-03-01 19:37:46 -0400

    testing movements

--- Extinction/Assets/PlayerMove.cs
+++ Extinction/Assets/PlayerMove.cs
@@ -12,9 +12,9 @@ public class PlayerMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		// rotate
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 20.0f;
 		// movement speed
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 20.0f;
 
         transform.Translate(x, 0, 0);
         transform.Translate(0, 0, z);
