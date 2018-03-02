77d1540796951dcb13f2d713168920e14d3fe6d9
Elliott-Siewert <elliott.siewert33@gmail.com>
2018-03-02 17:03:04 -0400

    hollow earth

---
+++ Extinction/Assets/Critter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Critter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y < -100){
+			Destroy(gameObject);
+			Debug.Log("Critter fell to hell");
+		}
+	}
+
+	void OnMouseDown(){
+		Destroy(gameObject);
+	}
+}
---
+++ Extinction/Assets/Scripts/PlayerAbilities.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilities : MonoBehaviour {
+
+	private Vector3 position;
+	public GameObject meteor;
+	public GameObject Plague;
+	private Camera c;
+
+	// Use this for initialization
+	void Start () {
+		c = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Meteor();
+		}
+	}
+
+	void Meteor () {
+		position = c.ScreenToWorldPoint(new Vector3(
+			Input.mousePosition.x,
+			Input.mousePosition.y,
+			20
+		));
+
+		Debug.Log("I CLICKED");
+		Instantiate(meteor, position, gameObject.transform.rotation);
+	}
+}
