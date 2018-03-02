ab0f164dae5dadfd21e1321bc3c30b0454ebbba7
Elliott-Siewert <elliott.siewert33@gmail.com>
2018-03-02 12:00:17 -0400

    player

---
+++ Extinction/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+	private Player instance;
+	private int power;
+	private Text txtPower;
+
+	//private constructor
+	private Player(){
+		power = 0;
+		
+	}
+
+	//get instance
+	public Player getInstance(){
+		return instance;
+	}
+
+	// Use this for initialization
+	void Start () {
+		instance = new Player();
+		instance.txtPower = GameObject.Find("lblPower").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		instance.txtPower.text = "Power: " + power;
+	}
+
+	//public methods
+	public int getPower(){
+		return instance.power;
+	}
+}
