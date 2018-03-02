13fb0c4766d48f21ae87c534c10862ec1e3f8bcc
Elliott-Siewert <elliott.siewert33@gmail.com>
2018-03-02 18:34:39 -0400

    consolidate

---
+++ Extinction/Assets/Scripts/PlagueScripts/Plagueis.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plagueis : MonoBehaviour {
+
+	public bool plague = false;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(plague);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// add but support here
+	}
+
+	public bool getPlague()
+	{
+		return plague;
+	}
+
+	public void setPlague()
+	{
+		plague = true;
+	}
+}
