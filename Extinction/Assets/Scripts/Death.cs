e3a6c43549935f12d69608a70624ab5595465b41
Dave-weatherby <dave-weatherby@hotmail.com>
2018-03-02 11:32:34 -0400

    age fix

---
+++ Extinction/Assets/Scripts/Death.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+	public float deathTime = 30;
+	private System.Timers.Timer age;
+	
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Countdown", deathTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private IEnumerator Countdown(int time){
+    while(time>0){
+        Debug.Log(time--);
+        yield return new WaitForSeconds(1);
+    }
+	Destroy (gameObject);
+    Debug.Log("Countdown Complete!");
+	}
+}
