8b38740c99beb850e48cf1fc054bd59421264fbb
Robert LeCreux <robertlecreux@hotmail.com>
2018-03-02 13:29:25 -0400

    mostly working meteors

---
+++ Extinction/Assets/Scripts/CreateMeteor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMeteor : MonoBehaviour {
+
+	private Vector3 position;
+	public GameObject gameObject;
+	private Camera c;
+
+	// Use this for initialization
+	void Start () {
+		c = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			
+			position = c.ScreenToWorldPoint(new Vector3(
+				Input.mousePosition.x,
+				Input.mousePosition.y,
+				20
+			));
+
+			Debug.Log("I CLICKED");
+			Instantiate(gameObject, position, gameObject.transform.rotation);
+		}
+
+	}
+
+}
---
+++ Extinction/Assets/Scripts/Meteor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour {
+
+	public GameObject myObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+        Instantiate(myObject, pos, rot);
+        Destroy(gameObject);
+    }
+}
