ffd815372583dc3c399f6fadc0b3b63b8dab7e52
Elliott-Siewert <elliott.siewert33@gmail.com>
2018-03-03 09:41:45 -0400

    laser explosions

--- Extinction/Assets/Laser.cs
+++ Extinction/Assets/Laser.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Laser : MonoBehaviour {
+	public GameObject fireBall;
 	private int count;
+	
 
 	// Use this for initialization
 	void Start () {
 		count = 0;
+		//Instantiate(fireBall, gameObject.transform);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		count ++;
-		if(count > 5){
+		if(count > 20){
 			Destroy(gameObject);
 		}
 	}
